fd3153e262b381cc04689d5124c7c84f21c23dcf
Pierre-mulliez1 <pierremulliez1@gmail.com>
2021-06-17 15:08:16 +0200

    new functions

--- src/ie_utils/__init__.py
+++ src/ie_utils/__init__.py
@@ -4,6 +4,9 @@ IE titanic utls
 
 __version__ = "0.1.0"
 
+from nltk.corpus import stopwords
+#nltk.download('stopwords')
+
 
 def tokenize(lis, lower = "F"):
     # Following function not used
@@ -20,6 +23,18 @@ def tokenize(lis, lower = "F"):
     # select directly the first element of system.arg
     return lis.split()
 
+
+def remove_stopwords(text):
+    text_tokens = tokenize(text)
+    stopwords = ["a", "the", "or", "and"]
+    tokens_without_sw = [word for word in text_tokens if not word in stopwords]
+    return tokens_without_sw
+
+def remove_punctuation(text):
+    text_tokens = tokenize(text)
+    punctuation = [".", ",", "!"]
+    tokens_without_punc = [word for word in text_tokens if not word in punctuation]
+    return tokens_without_punc
 
 if __name__ == "__main__":
     print(tokenize("am I splitted?"))
--- tests/test_ie_utils.py
+++ tests/test_ie_utils.py
@@ -1,4 +1,4 @@
-from ie_utils import tokenize
+from ie_utils import tokenize, remove_punctuation, remove_stopwords
 import pytest 
 
 @pytest.mark.parametrize("sentence, expected_tokens",[("first test sentence", ["first","test","sentence"]), ("a word",["a","word"])])
@@ -13,3 +13,21 @@ def test_tockenize_returns_lower(sentence, expected_tokens):
     for el in range(0, len(expected_tokens)):
         expected_tokens[el] =  expected_tokens[el].lower()
     assert tokens == expected_tokens
+
+@pytest.mark.parametrize("sentence, expected_tokens",[("first . test , sentence !", ["First","test","sentence"])])
+def test_tockenize_returns_no_punctation(sentence, expected_tokens):
+    tokens = remove_punctuation(sentence)
+    for el in range(0, len(expected_tokens)):
+        expected_tokens[el] =  expected_tokens[el].lower()
+    assert tokens == expected_tokens
+    
+@pytest.mark.parametrize("sentence, expected_tokens",[("the first test and sentence", ["First","test","sentence"])])
+def test_tockenize_returns_no_stopwords(sentence, expected_tokens):
+    tokens = remove_stopwords(sentence)
+    for el in range(0, len(expected_tokens)):
+        expected_tokens[el] =  expected_tokens[el].lower()
+    assert tokens == expected_tokens
+    
+    
+
+    
